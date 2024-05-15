da175453088857c5feea7a832315df76cb3f4716
balu1000 <v.bh.ukr.net@gmail.com>
2024-05-15 15:55:43 +0300

    change

--- variables.tf
+++ variables.tf
@@ -1,37 +1,37 @@
-variable "GOOGLE_PROJECT" {
-  type        = string
-  description = "GCP project name"
-}
+#variable "GOOGLE_PROJECT" {
+#  type        = string
+#  description = "GCP project name"
+#}
 
-variable "GOOGLE_REGION" {
-  type        = string
-  default     = "us-central1-c"
-  description = "GCP region to use"
-}
+#variable "GOOGLE_REGION" {
+#  type        = string
+#  default     = "us-central1-c"
+#  description = "GCP region to use"
+#}
 
-variable "GKE_NUM_NODES" {
-  type        = number
-  default     = 2
-  description = "GKE nodes number"
-}
+#variable "GKE_NUM_NODES" {
+#  type        = number
+#  default     = 2
+#  description = "GKE nodes number"
+#}
 
-variable "NODE_MACHINE_TYPE" {
-  type        = string
-  default     = "g1-small"
-  description = "Machine type for GKE nodes"
-}
+#variable "NODE_MACHINE_TYPE" {
+#  type        = string
+#  default     = "g1-small"
+#  description = "Machine type for GKE nodes"
+#}
 
-variable "GKE_CLUSTER_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE cluster name"
-}
+#variable "GKE_CLUSTER_NAME" {
+#  type        = string
+#  default     = "main"
+#  description = "GKE cluster name"
+#}
 
-variable "GKE_POOL_NAME" {
-  type        = string
-  default     = "main"
-  description = "GKE pool name"
-}
+#variable "GKE_POOL_NAME" {
+#  type        = string
+#  default     = "main"
+#  description = "GKE pool name"
+#}
 
 variable "GITHUB_OWNER" {
   type        = string
