f33d0969972a6388ca8dbb2678738d6a2666b2ca
balu1000 <v.bh.ukr.net@gmail.com>
2024-05-15 15:07:17 +0300

    change

---
+++ main.tf
@@ -0,0 +1,31 @@
+module "github_repository" {
+  source                   = "./modules/github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux0"
+}
+
+#module "gke_cluster" {
+#  source         = "./modules/google-gke-cluster"
+#  GOOGLE_REGION  = var.GOOGLE_REGION
+#  GOOGLE_PROJECT = var.GOOGLE_PROJECT
+#  GKE_NUM_NODES  = 1
+#}
+
+module "kind_cluster" {
+  source = "./modules/kind-cluster"
+}
+
+module "flux_bootstrap" {
+  source            = "./modules/fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.kind_cluster.kubeconfig
+}
+
+module "tls_private_key" {
+  source    = "./modules/hashicorp-tls-keys"
+  algorithm = "RSA"
+}
---
+++ variables.tf
@@ -0,0 +1,56 @@
+variable "GOOGLE_PROJECT" {
+  type = string
+  description = "GCP project name"
+}
+
+variable "GOOGLE_REGION" {
+  type = string
+  default = "us-central1-c"
+  description = "GCP region to use"
+}
+
+variable "GKE_NUM_NODES" {
+  type = number
+  default = 2
+  description = "GKE nodes number"
+}
+
+variable "NODE_MACHINE_TYPE" {
+  type = string
+  default = "g1-small"
+  description = "Machine type for GKE nodes"
+}
+
+variable "GKE_CLUSTER_NAME" {
+  type = string
+  default = "main"
+  description = "GKE cluster name"
+}
+
+variable "GKE_POOL_NAME" {
+  type = string
+  default = "main"
+  description = "GKE pool name"
+}
+
+variable "GITHUB_OWNER" {
+  type = string
+  description = "Github owner repository yo use"
+}
+
+variable "GITHUB_TOKEN" {
+  type = string
+  description = "Github personal access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+  type = string
+  default = "flux-gitops"
+  description = "Flux GitOps repository"
+}
+
+variable "FLUX_GITHUB_TARGET_PATH" {
+  type = string
+  default = "clusters"
+  description = "Flux manifests subdirectory"
+}
