b30c2b418a7de6d2f0552824134a0ffc8c789b0d
balu1000 <v.bh.ukr.net@gmail.com>
2024-05-15 21:42:32 +0300

    change

--- variables.tf
+++ variables.tf
@@ -1,35 +1,35 @@
 variable "GOOGLE_PROJECT" {
-  type = string
+  type        = string
   description = "GCP project name"
 }
 
 variable "GOOGLE_REGION" {
-  type = string
-  default = "us-central1-c"
+  type        = string
+  default     = "us-central1-c"
   description = "GCP region to use"
 }
 
 variable "GKE_NUM_NODES" {
-  type = number
-  default = 2
+  type        = number
+  default     = 2
   description = "GKE nodes number"
 }
 
 variable "NODE_MACHINE_TYPE" {
-  type = string
-  default = "g1-small"
+  type        = string
+  default     = "g1-small"
   description = "Machine type for GKE nodes"
 }
 
 variable "GKE_CLUSTER_NAME" {
-  type = string
-  default = "main"
+  type        = string
+  default     = "main"
   description = "GKE cluster name"
 }
 
 variable "GKE_POOL_NAME" {
-  type = string
-  default = "main"
+  type        = string
+  default     = "main"
   description = "GKE pool name"
 }
 
@@ -50,7 +50,7 @@ variable "FLUX_GITHUB_REPO" {
 }
 
 variable "FLUX_GITHUB_TARGET_PATH" {
-  type = string
-  default = "clusters"
+  type        = string
+  default     = "clusters"
   description = "Flux manifests subdirectory"
 }
