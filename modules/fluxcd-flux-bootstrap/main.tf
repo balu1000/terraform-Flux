c38722bd97e0b447502d9b2e7fc68e198f9a913e
Balu1000 <v.bh.ukr.net@gmail.com>
2024-05-16 10:47:40 +0000

    change

--- modules/fluxcd-flux-bootstrap/main.tf
+++ modules/fluxcd-flux-bootstrap/main.tf
@@ -5,7 +5,7 @@ provider "flux" {
   git = {
     url = "https://github.com/${var.github_repository}.git"
     http = {
-      username = "git"
+      username = "balu1000"
       password = var.github_token
     }
   }
