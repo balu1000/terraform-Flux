186e7fac2bb158af12d940b42055adcaeab4dbca
Balu1000 <v.bh.ukr.net@gmail.com>
2024-05-16 12:33:44 +0000

    change

--- modules/fluxcd-flux-bootstrap/variables.tf
+++ modules/fluxcd-flux-bootstrap/variables.tf
@@ -1,6 +1,6 @@
 variable "github_repository" {
   type        = string
-  default     = "flux-gitops"
+  default     = "balu1000/flux-gitops"
   description = "GitHub repository to store Flux manifests"
 }
 
